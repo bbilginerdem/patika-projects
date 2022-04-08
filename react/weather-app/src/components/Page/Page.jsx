950c191d1edb08051a9447dae7e3ed98efe41b7e
behzat bilgin erdem <bbilgin.erdem@gmail.com>
2022-04-09 00:01:51 +0300

    Weather api & design done

---
+++ react/weather-app/src/components/Page/Page.jsx
@@ -0,0 +1,38 @@
+import React, { Fragment } from 'react';
+
+import Header from '../Header';
+import Form from '../Form';
+import Error from '../Error';
+import Loader from '../Loader';
+import Forecast from '../Forecast';
+
+import useForecast from '../../hooks/useForecast'
+
+import styles from './Page.module.css';
+
+const Page = () => {
+    const { isError, isLoading, forecast, submitRequest } = useForecast();
+
+    const onSubmit = (value) => {
+        // call fn here
+        submitRequest(value);
+    }
+
+    return (
+        <Fragment>
+            <Header />
+            <div className={`${styles.box} position-relative`}>
+                {/* Form */}
+                {!isLoading && <Form submitSearch={onSubmit} />}
+                {/* Error */}
+                {isError && <Error message={isError} />}
+                {/* Loader */}
+                {isLoading && <Loader />}
+                {/* Forecast */}
+            </div>
+            {forecast && <Forecast />}
+        </Fragment>
+    );
+};
+
+export default Page;
