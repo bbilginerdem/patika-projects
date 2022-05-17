3581b84188d646cb8242b921fb45abe4ba94d84d
Behzat Bilgin Erdem <e200456@metu.edu.tr>
2022-05-17 14:19:45 +0300

    prime number algorithm with input

--- nodejs/primeNumbers.js
+++ nodejs/primeNumbers.js
@@ -1,3 +1,18 @@
+const arguments = process.argv.slice(2);
+
 function showPrimeNumbers(lowNumber, highNumber) {
-	for (let i = lowNumber; i <= highNumber; i++)
+	for (let i = lowNumber; i <= highNumber; i++) {
+		let isPrime = true;
+		for (let j = 2; j <= i; j++) {
+			if (i % j == 0 && i != j) {
+				isPrime = false;
+			}
+		}
+
+		if (isPrime) {
+			console.log(i);
+		}
+	}
 }
+
+showPrimeNumbers(parseInt(arguments[0]), parseInt(arguments[1]));
