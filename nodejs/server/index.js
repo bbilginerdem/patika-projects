0e93dc9c4cd7dffd5b918477a1b7e3ecd340b782
Behzat Bilgin Erdem <e200456@metu.edu.tr>
2022-05-22 13:59:26 +0300

    create server

---
+++ nodejs/server/index.js
@@ -0,0 +1,25 @@
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+	const url = req.url;
+	console.log("Request was made: " + url);
+
+	if (url === "/") {
+		res.writeHead(200, { "Context-Type": "text/html" });
+		res.write("<h2>Welcome to the homepage!</h2>");
+	} else if (url === "/about") {
+		res.writeHead(200, { "Context-Type": "text/html" });
+		res.write("<h2>Welcome to the about page!</h2>");
+	} else {
+		res.writeHead(404, { "Context-Type": "text/html" });
+		res.write("<h2>404 not found</h2>");
+	}
+
+	res.end();
+});
+
+const port = 5000;
+
+server.listen(port, () => {
+	console.log(`Listening on port ${port}`);
+});
