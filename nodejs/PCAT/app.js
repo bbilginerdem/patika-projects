620c3bcd2cefcb909a3fb7a7df6b270a3cd4a11d
Behzat Bilgin Erdem <e200456@metu.edu.tr>
2022-05-24 14:18:14 +0300

    creating given values for photos in database

--- nodejs/PCAT/app.js
+++ nodejs/PCAT/app.js
@@ -1,14 +1,26 @@
 const express = require("express");
+const mongoose = require("mongoose");
+
 const ejs = require("ejs");
 const path = require("path");
 
+const Photo = require("./models/Photo");
+
 const app = express();
+
+// connect DB
+mongoose.connect("mongodb://localhost/pcat-test-db", {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+});
 
 // TEMPLATE ENGINE
 app.set("view engine", "ejs");
 
 // MIDDLEWARES
 app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 // ROUTES
 app.get("/", (req, res) => {
@@ -21,6 +33,11 @@ app.get("/about", (req, res) => {
 
 app.get("/add-photo", (req, res) => {
 	res.render("add-photo");
+});
+
+app.post("/photos", async (req, res) => {
+	Photo.create(req.body);
+	res.redirect("/");
 });
 
 const port = 3000;
