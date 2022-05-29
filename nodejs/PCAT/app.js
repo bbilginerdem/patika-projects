fcbe9a0b097dabd0010161db0839bd47389e868c
behzat bilgin erdem <bbilgin.erdem@gmail.com>
2022-05-29 14:07:09 +0300

    photo & edit-photo pages added & middleware changed
    express fileupload & method override added for update and delete method simulations

--- nodejs/PCAT/app.js
+++ nodejs/PCAT/app.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const fileUpload = require("express-fileupload");
+const methodOverride = require("method-override");
+const fs = require("fs");
 
 const Photo = require("./models/Photo");
 
@@ -15,12 +18,25 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(fileUpload());
+app.use(
+	methodOverride("_method", {
+		methods: ["POST", "GET"],
+	})
+);
 
 // ROUTES
 app.get("/", async (req, res) => {
-	const photos = await Photo.find({});
+	const photos = await Photo.find({}).sort("-dateCreated");
 	res.render("index", {
 		photos: photos,
+	});
+});
+
+app.get("/photos/:id", async (req, res) => {
+	const photo = await Photo.findById(req.params.id);
+	res.render("photo", {
+		photo,
 	});
 });
 
@@ -32,12 +48,48 @@ app.get("/add-photo", (req, res) => {
 	res.render("add-photo");
 });
 
-app.post("/photos", async (req, res) => {
-	await Photo.create(req.body);
+app.get("/photos/edit/:id", async (req, res) => {
+	const photo = await Photo.findOne({ _id: req.params.id });
+	res.render("edit-photo", {
+		photo,
+	});
+});
+
+app.put("/photos/:id", async (req, res) => {
+	const photo = await Photo.findOne({ _id: req.params.id });
+	photo.title = req.body.title;
+	photo.description = req.body.description;
+	photo.save();
+
+	res.redirect(`${req.params.id}`);
+});
+
+app.delete("/photos/:id", async (req, res) => {
+	const photo = await Photo.findOne({ _id: req.params.id });
+	let deletedImage = __dirname + "/public" + photo.image;
+	fs.unlinkSync(deletedImage);
+	await Photo.findByIdAndRemove(req.params.id);
 	res.redirect("/");
 });
 
-const port = 3000;
-app.listen(port, () => {
-	console.log(`App listening on port ${port}!`);
+app.post("/photos", async (req, res) => {
+	const uploadDir = "public/uploads";
+
+	if (!fs.existsSync(uploadDir)) {
+		fs.mkdirSync(uploadDir);
+	}
+
+	let uploadedImage = req.files.image;
+	let uploadPath = __dirname + "/public/uploads/" + uploadedImage.name;
+
+	uploadedImage.mv(uploadPath, async () => {
+		await Photo.create({
+			...req.body,
+			image: "/uploads/" + uploadedImage.name,
+		});
+		res.redirect("/");
+	});
 });
+
+const port = 3000;
+app.listen(port, () => {});
